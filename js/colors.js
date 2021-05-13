1056fa9fd9b082505afe73aacfa1c3933f88da12
Likho1969 <likhokapesi04@gmail.com>
2021-05-13 16:45:58 +0200

    second commit

--- js/colors.js
+++ js/colors.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "block") {
-     
-    } else {
-      x.style.display = "block";
-    }
-  }
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
